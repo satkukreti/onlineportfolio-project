3555056ecc731576f68b0b3147324c884fe6098a
unknown <kdatt1@yahoo.com>
2020-10-15 22:23:18 -0400

    Added postgreSQL database to project

--- onlineportfolio/settings.py
+++ onlineportfolio/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'onlineportfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'onlineportfoliodb',
+        'USER':'postgres',
+        'PASSWORD':'PieLord2004!',
+        'HOST':'localhost',
+        'PORT':'5432', 
     }
 }
 
