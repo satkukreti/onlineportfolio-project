9bc88b53ca957cbb257c1eeb79c27ad5bff2d84a
unknown <kdatt1@yahoo.com>
2020-10-19 13:19:30 -0400

    Created work page

--- work/views.py
+++ work/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Work
 
 # Create your views here.
+def home(request):
+    works = Work.objects
+    return render(request, 'work/home.html', {'works':works})
