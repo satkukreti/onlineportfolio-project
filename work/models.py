df2edfbd6dc09d73be6407d2c0f0676bd70c2707
unknown <kdatt1@yahoo.com>
2020-10-27 12:45:30 -0400

    Working on blog interface

--- work/models.py
+++ work/models.py
@@ -2,5 +2,9 @@ from django.db import models
 
 # Create your models here.
 class Work(models.Model):
+    title = models.CharField(max_length = 250, default="")
     image = models.ImageField(upload_to='images/')
     summary = models.CharField(max_length = 250);
+
+    def __str__(self):
+        return self.title
--- work/views.py
+++ work/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+
 from .models import Work
 
 # Create your views here.
